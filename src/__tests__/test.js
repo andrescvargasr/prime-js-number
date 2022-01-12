b02d750ae246acca44c64d04bdeb95d663b68b41
andrescvargasr <andresc.vargasr@gmail.com>
2022-01-12 16:12:41 -0500

    feat: check for fisrt time pime number

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -2,6 +2,6 @@ import { myModule } from '..';
 
 describe('test myModule', () => {
   it('should return 42', () => {
-    expect(myModule()).toStrictEqual(42);
+    expect(myModule(2)).toStrictEqual(true);
   });
 });
--- src/index.js
+++ src/index.js
@@ -2,6 +2,34 @@
  * Returns a very important number
  * @return {number}
  */
-export function myModule() {
-  return 42;
+
+/**
+ * The primes are of the form 6k +- 1 with exception of 2 and 3.
+ * So, we can check if number is divisible by 2 or 3 and after that, check through all the mnumbers of form 6k +- 1
+ */
+
+export function myModule(numb) {
+  if (numb <= 1) {
+    return false;
+  }
+  if (numb <= 3) {
+    return true;
+  }
+  if (numb % 2 === 0 || numb % 3 === 0) {
+    return false;
+  }
+  for (let i = 5; i * i <= numb; i + 6) {
+    if (numb % i === 0 || numb % (i + 2) === 0 ) {
+      return false;
+    }
+  }
+
+  /*
+  for(let i = 2; i * i <= numb; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+  */
 }
