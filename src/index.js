d70ab504ea10c5c26e62ec5f8e15dc097e28dbd6
andrescvargasr <andresc.vargasr@gmail.com>
2022-01-18 11:33:44 -0500

    fix: for loop optimization

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ export function isPrimenumberer(number) {
   if (number % 2 === 0 || number % 3 === 0) {
     return false;
   }
-  for (let i = 5; i * i <= number; i + 6) {
+  for (let i = 5; i <= Math.sqrt(number); i + 6) {
     if (number % i === 0 || number % (i + 2) === 0 ) {
       return false;
     }
