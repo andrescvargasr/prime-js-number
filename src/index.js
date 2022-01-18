32be9602f6a20ff9791b40564e5836ce481104a7
andrescvargasr <andresc.vargasr@gmail.com>
2022-01-18 11:30:17 -0500

    fix: function name and variable name

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
 /**
- * Returns a very important number
- * @return {number}
+ * Returns a very important numberer
+ * @return {numberer}
  */
 
 /**
  * The primes are of the form 6k +- 1 with exception of 2 and 3.
- * So, we can check if number is divisible by 2 or 3 and after that, check through all the mnumbers of form 6k +- 1
+ * So, we can check if numberer is divisible by 2 or 3 and after that, check through all the mnumberers of form 6k +- 1
  */
 
-export function myModule(numb) {
-  if (numb <= 1) {
+export function isPrimenumberer(number) {
+  if (number <= 1) {
     return false;
   }
-  if (numb <= 3) {
+  if (number <= 3) {
     return true;
   }
-  if (numb % 2 === 0 || numb % 3 === 0) {
+  if (number % 2 === 0 || number % 3 === 0) {
     return false;
   }
-  for (let i = 5; i * i <= numb; i + 6) {
-    if (numb % i === 0 || numb % (i + 2) === 0 ) {
+  for (let i = 5; i * i <= number; i + 6) {
+    if (number % i === 0 || number % (i + 2) === 0 ) {
       return false;
     }
   }
 
   /*
-  for(let i = 2; i * i <= numb; i++) {
+  for(let i = 2; i * i <= number; i++) {
     if (num % i === 0) {
       return false;
     }
